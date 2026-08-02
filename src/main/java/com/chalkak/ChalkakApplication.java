d11c3cf2f8ed95e413a6bf09787e78782c810467
minjiwon <mjo4260@naver.com>
2026-08-02 16:28:13 +0900

    init: 최초 커밋

---
+++ src/main/java/com/chalkak/ChalkakApplication.java
@@ -0,0 +1,13 @@
+package com.chalkak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalkakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalkakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chalkak/ChalkakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chalkak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalkakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
