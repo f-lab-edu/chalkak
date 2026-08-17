f70a0fa7640b90a3a181e334ca7e8c0deadf9b6f
minjiwon <mjo4260@naver.com>
2026-08-17 15:02:04 +0900

    feat: PointController 인증 정보에서 userId 조회하도록 변경

--- src/main/java/com/chalkak/point/controller/PointController.java
+++ src/main/java/com/chalkak/point/controller/PointController.java
@@ -1,15 +1,16 @@
 package com.chalkak.point.controller;
 
+import com.chalkak.auth.principal.AuthUserPrincipal;
 import com.chalkak.point.controller.request.PointRequest;
 import com.chalkak.point.controller.response.PointResponse;
 import com.chalkak.point.service.PointService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,13 +20,12 @@ public class PointController {
 
     private final PointService pointService;
 
-    // TODO: Security 머지 후 @AuthenticationPrincipal AuthUserPrincipal에서 userId를 꺼내는 방식으로 교체
     @PostMapping("/charge")
     public ResponseEntity<PointResponse> charge(
-        @RequestParam Long userId,
+        @AuthenticationPrincipal AuthUserPrincipal principal,
         @Valid @RequestBody PointRequest request
     ) {
-        PointResponse response = pointService.charge(userId, request.amount());
+        PointResponse response = pointService.charge(principal.getUserId(), request.amount());
         return ResponseEntity.ok(response);
     }
 }
