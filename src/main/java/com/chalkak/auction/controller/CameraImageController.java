642b75790195507f3071359f16487c5665437d8c
minjiwon <mjo4260@naver.com>
2026-09-14 21:53:36 +0900

    feat: 카메라 이미지 다운로드 API에 캐싱 정책 적용

--- src/main/java/com/chalkak/auction/controller/CameraImageController.java
+++ src/main/java/com/chalkak/auction/controller/CameraImageController.java
@@ -3,8 +3,10 @@ package com.chalkak.auction.controller;
 import com.chalkak.auction.service.CameraImageDownload;
 import com.chalkak.auction.service.CameraImageService;
 import com.chalkak.common.util.ImageUrls;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +24,7 @@ public class CameraImageController {
     public ResponseEntity<Resource> download(@PathVariable Long imageId) {
         CameraImageDownload download = cameraImageService.download(imageId);
         return ResponseEntity.ok()
+            .cacheControl(CacheControl.maxAge(Duration.ofDays(30)).cachePublic())
             .contentType(download.contentType())
             .body(download.resource());
     }
